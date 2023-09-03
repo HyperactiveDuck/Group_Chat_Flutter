041737c5ef429c6e31d2fee9e3bb4ded2bbbbdb7
Yücel Arda DEMİRCİ <demirciyucelarda@gmail.com>
2023-09-03 05:37:50 +0300

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:flash_flutter/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_flutter/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -74,7 +75,7 @@ class ChatScreenState extends State<ChatScreen> {
                   showSpinner = true;
                 });
                 await _auth.signOut();
-                Navigator.pop(context);
+                Navigator.popAndPushNamed(context, WelcomeScreen.id);
                 setState(() {
                   showSpinner = false;
                 });
